a96a4a59e3fe74be00dff17640c0a584ae87830b
Carol-Bitencourt <carolmedeirosbitencourt@gmail.com>
2020-11-25 23:09:53 -0300

    Exercicio 1, operação aritmética básica

---
+++ exercises/4_1/exercicios1.js
@@ -0,0 +1,10 @@
+let a = 10;
+let b = 2;
+
+console.log('adição', a + b)
+console.log('subtração', a - b)
+console.log('multiplicação', a * b)
+console.log('divisão', a / b)
+console.log('módulo', a % b)
+
+
