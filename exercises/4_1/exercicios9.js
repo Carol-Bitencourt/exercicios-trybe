31d207b4a5a6d1152fe82fe7b45ac52fcea55464
Carol-Bitencourt <carolmedeirosbitencourt@gmail.com>
2020-11-29 13:30:43 -0300

    Exercicio 9, condicionais

---
+++ exercises/4_1/exercicios9.js
@@ -0,0 +1,12 @@
+// Escreva um programa que defina três números em variáveis no seu começo e retorne true
+//se pelo menos uma das três for ímpar. Caso contrário, ele retorna false .
+// Bonus: use somente um if.
+
+let a = 2;
+let b = 5;
+let c = 10;
+if ((a % 2 == 1) || (b % 2 == 1) || (c % 2 == 1)) {
+    console.log(true);
+} else {
+    console.log(false);
+}
